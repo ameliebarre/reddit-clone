eb4cd661902fb973bc05a3aa82fc6d05b70b13bc
Amelie <amelie.barre.co@gmail.com>
2023-12-04 22:58:01 +0100

    sign in, sign out and checking auth status

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,28 @@
 import { Button } from "@nextui-org/react";
+import * as actions from "@/actions";
+import { auth } from "@/auth";
+import Profile from "@/components/Profile";
 
-export default function Home() {
-  return <Button>Click me !</Button>;
+export default async function Home() {
+  const session = await auth();
+
+  return (
+    <>
+      <form action={actions.signIn}>
+        <Button type="submit">Sign In</Button>
+      </form>
+
+      <form action={actions.signOut}>
+        <Button type="submit">Sign Out</Button>
+      </form>
+
+      {session?.user ? (
+        <div>{JSON.stringify(session.user)}</div>
+      ) : (
+        <div>Signed Out</div>
+      )}
+
+      <Profile />
+    </>
+  );
 }
